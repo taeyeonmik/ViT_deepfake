8d12b2a2a0a9dca3ef3e95431d91ea787dfe28ac
taeyeonmik <bigkitekim@icloud.com>
2024-05-09 16:34:38 +0200

    Modifying DeepfakeDataset Class

---
+++ efficient-vit/dataset.py
@@ -0,0 +1,104 @@
+import torch
+from torch.utils.data import DataLoader, TensorDataset, Dataset
+import cv2
+import numpy as np
+from PIL import Image
+
+import uuid
+from albumentations import Compose, RandomBrightnessContrast, \
+    HorizontalFlip, FancyPCA, HueSaturationValue, OneOf, ToGray, \
+    ShiftScaleRotate, ImageCompression, PadIfNeeded, GaussNoise, GaussianBlur, Rotate
+from albumentations import DualTransform, ImageOnlyTransform
+#from albumentations.augmentations.functional import crop
+
+
+def isotropically_resize_image(img, size, interpolation_down=cv2.INTER_AREA, interpolation_up=cv2.INTER_CUBIC):
+    h, w = img.shape[:2]
+
+    if max(w, h) == size:
+        return img
+    if w > h:
+        scale = size / w
+        h = h * scale
+        w = size
+    else:
+        scale = size / h
+        w = w * scale
+        h = size
+    interpolation = interpolation_up if scale > 1 else interpolation_down
+
+    img = img.astype('uint8')
+    resized = cv2.resize(img, (int(w), int(h)), interpolation=interpolation)
+    return resized
+
+
+class IsotropicResize(DualTransform):
+    def __init__(self, max_side, interpolation_down=cv2.INTER_AREA, interpolation_up=cv2.INTER_CUBIC,
+                 always_apply=False, p=1):
+        super(IsotropicResize, self).__init__(always_apply, p)
+        self.max_side = max_side
+        self.interpolation_down = interpolation_down
+        self.interpolation_up = interpolation_up
+
+    def apply(self, img, interpolation_down=cv2.INTER_AREA, interpolation_up=cv2.INTER_CUBIC, **params):
+        return isotropically_resize_image(img, size=self.max_side, interpolation_down=interpolation_down,
+                                          interpolation_up=interpolation_up)
+
+    def apply_to_mask(self, img, **params):
+        return self.apply(img, interpolation_down=cv2.INTER_NEAREST, interpolation_up=cv2.INTER_NEAREST, **params)
+
+    def get_transform_init_args_names(self):
+        return ("max_side", "interpolation_down", "interpolation_up")
+
+
+class DeepFakesDataset(Dataset):
+    def __init__(self, images, labels, image_size, mode='train'):
+        self.x = images
+        self.y = torch.from_numpy(labels)
+        self.image_size = image_size
+        self.mode = mode
+        self.n_samples = len(images)
+
+    def create_train_transforms(self, size):
+        return Compose([
+            ImageCompression(quality_lower=60, quality_upper=100, p=0.2),
+            GaussNoise(p=0.3),
+            # GaussianBlur(blur_limit=3, p=0.05),
+            HorizontalFlip(),
+            OneOf([
+                IsotropicResize(max_side=size, interpolation_down=cv2.INTER_AREA, interpolation_up=cv2.INTER_CUBIC),
+                IsotropicResize(max_side=size, interpolation_down=cv2.INTER_AREA, interpolation_up=cv2.INTER_LINEAR),
+                IsotropicResize(max_side=size, interpolation_down=cv2.INTER_LINEAR, interpolation_up=cv2.INTER_LINEAR),
+            ], p=1),
+            PadIfNeeded(min_height=size, min_width=size, border_mode=cv2.BORDER_CONSTANT),
+            OneOf([RandomBrightnessContrast(), FancyPCA(), HueSaturationValue()], p=0.4),
+            ToGray(p=0.2),
+            ShiftScaleRotate(shift_limit=0.1, scale_limit=0.2, rotate_limit=5, border_mode=cv2.BORDER_CONSTANT, p=0.5),
+        ]
+        )
+
+    def create_val_transform(self, size):
+        return Compose([
+            IsotropicResize(max_side=size, interpolation_down=cv2.INTER_AREA, interpolation_up=cv2.INTER_CUBIC),
+            PadIfNeeded(min_height=size, min_width=size, border_mode=cv2.BORDER_CONSTANT),
+        ])
+
+    def __getitem__(self, index):
+        image = np.asarray(Image.open(self.x[index]))
+
+        if self.mode == 'train':
+            transform = self.create_train_transforms(self.image_size)
+        else:
+            transform = self.create_val_transform(self.image_size)
+
+        unique = uuid.uuid4()
+        # cv2.imwrite("../dataset/augmented_frames/isotropic_augmentation/"+str(unique)+"_"+str(index)+"_original.png", image)
+
+        image = transform(image=image)['image']
+
+        # cv2.imwrite("../dataset/augmented_frames/isotropic_augmentation/"+str(unique)+"_"+str(index)+".png", image)
+
+        return torch.tensor(image).float(), self.y[index]
+
+    def __len__(self):
+        return self.n_samples
